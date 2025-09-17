af4c89a79c968e407aacf0c44df6411bf036f8eb
Shweta Jadhav <shwetajadhav2324@gmail.com>
2025-09-17 18:43:43 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,5 @@
+// Show contact cards when "Get in Touch" button is clicked
+document.getElementById('showContactBtn').addEventListener('click', function() {
+    document.getElementById('contactCards').style.display = 'block';
+    this.style.display = 'none';
+});
